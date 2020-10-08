bcdd0b426f06d570cd6ed5802ddc18e6c0cae229
曹兴庄 <774827571@qq.com>
2020-10-08 20:14:22 +0800

    后台主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,17 @@ var baseURL = 'http://ajax.frontend.itheima.net';
 
 $.ajaxPrefilter(function (params) {
     params.url = baseURL + params.url;
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 拦截未登录禁止访问有权限的页面
+    params.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = 'login.html';
+        }
+    }
 })
